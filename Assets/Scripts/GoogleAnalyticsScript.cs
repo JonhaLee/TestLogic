ba9061d64a1a406d0605a355bfef83239b97d780
nairoon <nairoon@naver.com>
2015-03-26 13:13:36 +0900

    Test

---
+++ Assets/Scripts/GoogleAnalyticsScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GoogleAnalyticsScript : MonoBehaviour {
+    public GoogleAnalyticsV3 googleAnalytics;
+	// Use this for initialization
+	void Start () {
+        if (GameObject.Find("StaticObject") == null)
+        {
+            Debug.LogError("Not Exist StaticObject for google analytics and admob");
+            
+        }
+        else
+        {
+            googleAnalytics = GameObject.Find("GAv3").GetComponent<GoogleAnalyticsV3>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/NormalMode.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+/* Log
+ * 게임 모드 중 가장 기본적인 표시된 카드와 나열 된 카드를 맞추는 모드
+ * 생성자 : 이존하
+ * 작업목록 
+ * 2015.03.15 - 생성
+ */
+public class NormalMode : GameMode {
+
+	
+}
